4da4c78bac7d9576d28546d8cc07a4f2fe5ee2ba
probablyvivek <viv.tiwari13@gmail.com>
2025-01-07 22:57:53 +0530

    First Question

--- SQL-Projects/PGExercises/Basics.sql
+++ SQL-Projects/PGExercises/Basics.sql
@@ -1,3 +1,5 @@
 -- Retrieve all information from the cd.facilities table
 SELECT * FROM cd.facilities;
 
+-- You want to print out a list of all of the facilities and their cost to members. How would you retrieve a list of only facility names and costs?
+SELECT name, membercost FROM cd.facilities;
